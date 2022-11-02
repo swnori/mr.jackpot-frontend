f5b92360e8a4252bfa6cab8ca5de6e5825883608
LeeMir <manofgodmj@gmail.com>
2022-11-02 14:02:39 +0900

    [Feat] 스크롤 방향에 따라 Footer 숨김처리

--- src/components/Frame/ClientFrame/index.tsx
+++ src/components/Frame/ClientFrame/index.tsx
@@ -1,4 +1,6 @@
 import { Outlet, useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { AnimatePresence } from 'framer-motion';
 
 import { ClientFrameContainer } from './style';
 
@@ -7,11 +9,45 @@ import { ClientFooter } from '@/components/Footer';
 
 const ClientFrame = () => {
   const location = useLocation();
+
+  const [isScrollUp, setIsScrollUp] = useState<boolean>(true);
+
+  useEffect(() => {
+    const threshold = 0;
+    let lastScrollY = window.pageYOffset;
+    let ticking = false;
+
+    const updateScrollDir = () => {
+      const scrollY = window.pageYOffset;
+
+      if (Math.abs(scrollY - lastScrollY) < threshold) {
+        ticking = false;
+        return;
+      }
+      setIsScrollUp(scrollY < lastScrollY);
+      lastScrollY = scrollY > 0 ? scrollY : 0;
+      ticking = false;
+    };
+
+    const onScroll = () => {
+      if (!ticking) {
+        window.requestAnimationFrame(updateScrollDir);
+        ticking = true;
+      }
+    };
+
+    window.addEventListener('scroll', onScroll);
+
+    return () => window.removeEventListener('scroll', onScroll);
+  }, [isScrollUp]);
+
   return (
     <Page type='client'>
       <ClientFrameContainer>
         <Outlet />
-        <ClientFooter path={location.pathname} />
+        <AnimatePresence exitBeforeEnter>
+          {isScrollUp ? <ClientFooter path={location.pathname} /> : null}
+        </AnimatePresence>
       </ClientFrameContainer>
     </Page>
   );
