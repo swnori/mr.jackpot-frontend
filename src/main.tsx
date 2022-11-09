d928d1eea1e4cae167b62fc2bae11df5c415c0d2
LeeMir <manofgodmj@gmail.com>
2022-11-09 16:24:14 +0900

    [Chore] install react-query

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,13 @@
+import { QueryClient, QueryClientProvider } from 'react-query';
 import ReactDOM from 'react-dom/client';
 import React from 'react';
 
 import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />);
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>,
+);
