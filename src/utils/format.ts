afcdf65eb1fd8e4b35699c866ff88a16771a2df6
LeeMir <manofgodmj@gmail.com>
2022-11-16 10:41:57 +0900

    [Feat] digitFormat 함수 작성

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,1 +1,13 @@
 export const KRWFormat = (num: number) => `KRW ${num.toLocaleString('ko-KR')}`;
+
+/**
+ * @param num 숫자를
+ * @param dight 몇 자리수로 나타낼 건지 (default = 1)
+ * @returns 문자열로
+ */
+export const digitFormat = (num: number, digit: number = 1) => {
+  const ten = 10 ** (digit - 1);
+  const zeros = '0'.repeat(digit - num.toString().length);
+
+  return num < ten ? `${zeros}${num}` : `${num}`;
+};
