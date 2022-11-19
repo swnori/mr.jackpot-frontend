5b94af145427946059c5e796109aa7fc90339564
LeeMir <manofgodmj@gmail.com>
2022-11-20 03:22:37 +0900

    [Chore] 직원 로그인 시, 모바일이면 배달로 이동

--- src/pages/staff/StaffLoginPage/index.tsx
+++ src/pages/staff/StaffLoginPage/index.tsx
@@ -1,3 +1,5 @@
+import { isMobile } from 'react-device-detect';
+
 import { LoginContainer } from './style';
 
 import Page from '@/components/Page';
@@ -12,13 +14,20 @@ import StaffIcon from '@/assets/icons/icon-chef.svg';
 
 const StaffLoginPage = () => {
   const link = useLink();
+  const loginHandler = () => {
+    if (isMobile) {
+      link.to('/staff/delivery/main');
+    } else {
+      link.to('/staff/cook/order');
+    }
+  };
   return (
     <Page type='common'>
       <LoginContainer>
         <LoginCard title='환영합니다!' img={StaffIcon}>
           <LoginCardDesc>직원코드를 입력하세요.</LoginCardDesc>
           <DesktopCardInput onChange={() => {}} placeholder='직원코드 입력' img={IDCardIcon} />
-          <DesktopCardBtn onClick={() => link.to('/staff/delivery/main')}>로그인</DesktopCardBtn>
+          <DesktopCardBtn onClick={loginHandler}>로그인</DesktopCardBtn>
         </LoginCard>
       </LoginContainer>
     </Page>
