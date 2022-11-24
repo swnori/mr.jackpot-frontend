86b55bb5306751b1cb6fff24f713dda0c137595b
LeeMir <manofgodmj@gmail.com>
2022-11-25 03:20:34 +0900

    [Chore] react-toastify 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import 'regenerator-runtime/runtime';
 import { RecoilRoot } from 'recoil';
+import { ToastContainer } from 'react-toastify';
+import { isMobile } from 'react-device-detect';
 import React from 'react';
 
 import ModalRoot from './components/Modal/ModalRoot';
@@ -9,6 +11,7 @@ import GlobalStyle from '@/styles/global';
 import Router from '@/routes';
 
 import '@/styles/fonts.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
@@ -17,6 +20,7 @@ const App = () => {
         <React.Suspense fallback={<Loading />}>
           <Router />
           <ModalRoot />
+          <ToastContainer position={isMobile ? 'top-center' : 'top-right'} autoClose={3000} />
         </React.Suspense>
       </RecoilRoot>
       <GlobalStyle />
