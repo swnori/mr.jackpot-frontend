aa04ba7ea456abba4751f0ce8074996e42e33137
LeeMir <manofgodmj@gmail.com>
2022-11-28 04:26:43 +0900

    [Refactor] state api 변경에 따른 수정

--- src/stores/state.ts
+++ src/stores/state.ts
@@ -1,23 +1,6 @@
 import { selector } from 'recoil';
 
-import { KeyOf } from '@/utils/type';
-
 import { fetchState } from '@/apis/common';
-
-const stateDic =
-  {
-    created: '대기',
-    rejected: '취소',
-    accepted: '접수',
-    started: '접수',
-    cooking: '요리',
-    prepared: '포장',
-    delivering: '배달',
-    delivered: '도착',
-    requested: '회수',
-    collected: '회수',
-    finished: '완료',
-  } ?? '';
 
 export const stateState = selector({
   key: 'stateState',
@@ -27,7 +10,7 @@ export const stateState = selector({
     const dict = data.reduce((pre: any, cur: any) => {
       const { id, state } = cur;
       const next = { ...pre };
-      next[id] = { id, name: stateDic[state as KeyOf<typeof stateDic>] };
+      next[id] = { id, name: state };
       return next;
     }, {} as { [key: number]: { id: number; name: string } });
 
